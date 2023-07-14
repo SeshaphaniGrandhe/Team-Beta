055d64e0f058dfd64424fdf7f32874787eb8a862
SeshaphaniGrandhe <133180827+SeshaphaniGrandhe@users.noreply.github.com>
2023-07-15 03:04:30 +0530

    Logic of Quiz function
    Picks 10 random questions from .csv file and display it then allows the user to choose from the options and compare it with the answer and finally shows result after submission of quiz.

--- app.py
+++ app.py
@@ -71,6 +71,71 @@ def dashboard():
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
+    if request.method == 'POST':
+        # Retrieve the selected answers from the form
+        selected_answers = []
+        for i in range(1, 11):
+            answer = request.form.get(f'question_{i}')
+            selected_answers.append(answer)
+
+        # Store the selected answers in the session
+        session['selected_answers'] = selected_answers
+
+        try:
+            # Load the trained model from the .pkl file
+            with open('random_forest_model.pkl', 'rb') as f:
+                model = pickle.load(f)
+
+            # Transform the selected answers using the CountVectorizer
+            selected_answers_bow = vectorizer.transform(selected_answers)
+
+            # Perform predictions using the trained model
+            predicted_answers = model.predict(selected_answers_bow)
+
+            # Evaluate the answers and calculate the score
+            score = calculate_score(predicted_answers)
+
+        except:
+            # Fall back to comparing selected answers with CSV file's correct answers
+            # Read the correct answers from the CSV file
+            correct_answers = []
+            with open('updated_file.csv', 'r') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    correct_answers.append(row['Answer'])
+
+            # Evaluate the answers and calculate the score
+            score = 0
+            for selected, correct in zip(selected_answers, correct_answers):
+                if selected == correct:
+                    score += 1
+
+        # Store the score in the session
+        session['score'] = score
+
+        # Redirect to the result page
+        return redirect(url_for('result'))
+
+    # Check if there are any previously selected answers in the session
+    selected_answers = session.get('selected_answers', [])
+
+    # Read the quiz questions from the CSV file
+    questions = []
+    with open('updated_file.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            options = [value for key, value in row.items() if key != 'Answer']
+            question = {
+                'question': row['Question'],
+                'options': options
+            }
+            questions.append(question)
+
+    # Select 10 random questions
+    selected_questions = random.sample(questions, 10)
+
+    return render_template('quiz.html', questions=selected_questions, selected_answers=selected_answers)
+
     
     return interpretation
 
